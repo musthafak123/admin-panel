5eef82c5f8c9ef724e4828c8703a53a506b8f7c2
musthafa_kareechiyil <musthafakareechiyil@gmail.com>
2022-12-23 21:06:43 +0530

    admin access

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,4 +1,7 @@
 RailsAdmin.config do |config|
+  config.authorize_with do
+    redirect_to main_app,root_path unless warden.user.admin == true
+  end
   config.asset_source = :sprockets
 
   ### Popular gems integration
